7181802d09c7da701d64f5a47c2e90c783ec3472
sange371 <zusangembaleki@icloud.com>
2025-07-14 14:59:41 +0200

    changed city names

--- javascript/functioning.js
+++ javascript/functioning.js
@@ -1,35 +1,36 @@
 function updateTheTime(){
     
-    let mexicoCityElement=document.querySelector("#mexico-city");
-    let seoulElement=document.querySelector("#seoul");
+    let mexicoCityElement=document.querySelector("#mexico");
+    let parisElement=document.querySelector("#paris");
     let londonElement=document.querySelector("#london");
-    if (mexicoCityElement || seoulElement || londonElement){
+
 
         let mexicoCityDate=mexicoCityElement.querySelector(".date")
         let mexicoCityTime=mexicoCityElement.querySelector(".time");
-        let seoulDate=seoulElement.querySelector("#date");
-        let seoulTime=seoulElement.querySelector("#time");
-        let londonDate=londonElement.querySelector("#date");
-        let londonTime=londonElement.querySelector("#time");
+        let parisDate=parisElement.querySelector(".date");
+        let parisTime=parisElement.querySelector(".time");
+        let londonDate=londonElement.querySelector(".date");
+        let londonTime=londonElement.querySelector(".time");
 
         let mexicoTime=moment().tz("America/Mexico_City");
-        let seoul=moment().tz("Asia/Seoul");
+        let paris=moment().tz("Asia/Seoul");
         let london=moment().tz("europe/London");
 
         mexicoCityDate.innerHTML=mexicoTime.format("MMMM Do YYYY");
         mexicoCityTime.innerHTML=mexicoTime.format("h:mm:ss [<small>]A[</small>]");
-        seoulDate.innerHTML=seoul.format("MMMM Do YYYY");
-        seoulTime.innerHTML=seoul.format("h:mm:ss [<small>]A[</small>]");
+        parisDate.innerHTML=paris.format("MMMM Do YYYY");
+        parisTime.innerHTML=paris.format("h:mm:ss [<small>]A[</small>]");
         londonDate.innerHTML=london.format("MMMM Do YYYY");
         londonTime.innerHTML=london.format("h:mm:ss [<small>]A[</small>]");
 
 
     }
     
-}
+
 
 
 updateTheTime();
+setInterval(updateTheTime,1000);
 
 
 
