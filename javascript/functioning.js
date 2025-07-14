024a50d45b52e42065ff844884944aa4d3b23913
sange371 <zusangembaleki@icloud.com>
2025-07-14 15:32:40 +0200

    changed the height of the container

--- javascript/functioning.js
+++ javascript/functioning.js
@@ -1,32 +1,20 @@
 function updateTheTime(){
     
-    let mexicoCityElement=document.querySelector("#mexico");
-    let parisElement=document.querySelector("#paris");
-    let londonElement=document.querySelector("#london");
-
-
-        let mexicoCityDate=mexicoCityElement.querySelector(".date")
-        let mexicoCityTime=mexicoCityElement.querySelector(".time");
-        let parisDate=parisElement.querySelector(".date");
-        let parisTime=parisElement.querySelector(".time");
-        let londonDate=londonElement.querySelector(".date");
-        let londonTime=londonElement.querySelector(".time");
-
-        let mexicoTime=moment().tz("America/Mexico_City");
-        let paris=moment().tz("Asia/Seoul");
-        let london=moment().tz("europe/London");
-
-        mexicoCityDate.innerHTML=mexicoTime.format("MMMM Do YYYY");
-        mexicoCityTime.innerHTML=mexicoTime.format("h:mm:ss [<small>]A[</small>]");
-        parisDate.innerHTML=paris.format("MMMM Do YYYY");
-        parisTime.innerHTML=paris.format("h:mm:ss [<small>]A[</small>]");
-        londonDate.innerHTML=london.format("MMMM Do YYYY");
-        londonTime.innerHTML=london.format("h:mm:ss [<small>]A[</small>]");
-
-
-    }
     
+    let parisElement=document.querySelector("#paris");
 
+    let parisDate=parisElement.querySelector(".date");
+    let parisTime=parisElement.querySelector(".time");
+              
+    let paris=moment().tz("Asia/Seoul");
+               
+    parisDate.innerHTML=paris.format("MMMM Do YYYY");
+    parisTime.innerHTML=paris.format("h:mm:ss [<small>]A[</small>]");
+        
+
+
+}
+    
 
 
 updateTheTime();
@@ -53,9 +41,12 @@ function updateTheCityTime(event){
                              </div>
                                 `;
 
+    
+
 }
 
 let citiesSelector=document.querySelector("#city-selector");
 citiesSelector.addEventListener("change", updateTheCityTime);
+
 
 setInterval(updateTheCityTime , 1000);
